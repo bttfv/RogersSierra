e91c2adabf2f2eeb24b3fa8683c1c6ea3c6e8799
bttf92 <lucadigiovanni@outlook.it>
2021-02-21 20:53:46 +0100

    Moved enums to correct class

--- Shared/TrainExtensions.vb
+++ Shared/TrainExtensions.vb
@@ -12,4 +12,35 @@ Public Module TrainExtensions
 
         Return vehicle.Model = TrainModels.RogersSierraColModel Or vehicle.Model = TrainModels.RogersSierraModel
     End Function
+
+    Public Enum SmokeColor
+
+        Off
+        [Default]
+        Green
+        Yellow
+        Red
+    End Enum
+
+    Public Enum TrainCamera
+
+        Off = -1
+        TowardsRail
+        Pilot
+        Front
+        RightFunnel
+        RightWheels
+        RightFrontWheels
+        RightFront2Wheels
+        RightSide
+        TopCabin
+        LeftSide
+        LeftFunnel
+        LeftWheels
+        LeftFrontWheels
+        LeftFront2Wheels
+        Inside
+        WheelieUp
+        WheelieDown
+    End Enum
 End Module
